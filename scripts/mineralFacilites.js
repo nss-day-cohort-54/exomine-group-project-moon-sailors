f21aaaedd38de685bfb9e687a1875bb1f636ffcd
Jaimie Hart <hartjme@gmail.com>
2022-02-01 15:08:11 -0500

    Working on facility dropdown

--- scripts/mineralFacilites.js
+++ scripts/mineralFacilites.js
@@ -3,10 +3,17 @@ import { getMineralFacilites } from "./dataAccess.js"
 
 const mineralFacilitiesArray = getMineralFacilites()
 
+const facilityMatch = (facilityObject) => {
+    if (facilityObject.id === facilityId) {
+        return true
+    }
+    return false
+}
+
 //Filter the array of facility minerals on the facilityId foreign key as the first step. 
+//It should match the id of the facility chosen by the user.
 export const facilityMineralContent = () => {
-    const filteredFacilityMinerals = mineralFacilitiesArray.filter(facilityId)
+    const filteredFacilityMinerals = mineralFacilitiesArray.filter(facilityMatch)
 
     return filteredFacilityMinerals
 }
-//It should match the id of the facility chosen by the user.
